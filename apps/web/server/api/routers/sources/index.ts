f33c83edff395d9b4fec42c38018eac191471569
SaurabhJain708 <saurabhanchal381@gmail.com>
2026-01-28 02:17:28 +0530

    Added file uploading trpc endpoint

--- apps/web/server/api/routers/sources/index.ts
+++ apps/web/server/api/routers/sources/index.ts
@@ -1,6 +1,8 @@
 import { createTRPCRouter } from "../../trpc";
 import { getSources } from "./get-sources";
+import { uploadFile } from "./upload-file";
 
 export const sourcesRouter = createTRPCRouter({
   getSources: getSources,
+  uploadFile: uploadFile,
 });
---
+++ apps/web/server/api/routers/sources/upload-file.ts
@@ -0,0 +1,72 @@
+import { TRPCError } from "@trpc/server";
+import { protectedProcedure } from "../../trpc";
+import { z } from "zod";
+import { FileProcessingStatus, FileType } from "@repo/db";
+import { uploadFile as uploadFileToStorage } from "@/lib/upload-file";
+import { supabase } from "@/lib/supabase";
+import { redis } from "@/lib/redis";
+
+export const uploadFile = protectedProcedure
+  .input(
+    z.object({
+      fileBase64: z.string(),
+      fileName: z.string(),
+      notebookId: z.string(),
+    })
+  )
+  .mutation(async ({ ctx, input }) => {
+    const { fileBase64, fileName, notebookId } = input;
+    const userId = ctx.session.user.id;
+    const notebook = await ctx.db.notebook.findUnique({
+      where: {
+        id: notebookId,
+        userId,
+      },
+    });
+    if (!notebook) {
+      throw new TRPCError({ code: "NOT_FOUND", message: "Notebook not found" });
+    }
+    const buffer = Buffer.from(fileBase64, "base64");
+    const data = await uploadFileToStorage({
+      buffer,
+      mimeType: "application/pdf",
+      userId,
+    });
+
+    const { data: signedUrlData } = await supabase.storage
+      .from("files")
+      .createSignedUrl(data.path, 3600);
+    const signedUrl = signedUrlData?.signedUrl || null;
+    if (!signedUrl) {
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Failed to get signed URL",
+      });
+    }
+
+    await redis.lpush(
+      "file_processing_queue",
+      JSON.stringify({
+        id: data.id,
+        userId,
+        notebookId,
+        fileName,
+        mimeType: "application/pdf",
+        signedUrl,
+      })
+    );
+
+    const source = await ctx.db.source.create({
+      data: {
+        id: data.id,
+        userId,
+        notebookId,
+        name: fileName,
+        type: FileType.pdf,
+        path: data.path,
+        processingStatus: FileProcessingStatus.queued,
+      },
+    });
+
+    return { success: true, sourceId: source.id };
+  });
