6b7cd76710bbac240f95b28235d43024ab65632c
SaurabhJain708 <saurabhanchal381@gmail.com>
2026-01-31 05:25:15 +0530

    Fixed prisma version conflicts

---
+++ packages/database/prisma/migrations/20260130235115_added_pgvector/migration.sql
@@ -0,0 +1,81 @@
+/*
+  Warnings:
+
+  - You are about to drop the `file` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- CreateExtension
+CREATE EXTENSION IF NOT EXISTS "vector";
+
+-- DropForeignKey
+ALTER TABLE "file" DROP CONSTRAINT "file_notebookId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "file" DROP CONSTRAINT "file_userId_fkey";
+
+-- DropTable
+DROP TABLE "file";
+
+-- CreateTable
+CREATE TABLE "Source" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "path" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+    "content" JSONB,
+    "processingStatus" "FileProcessingStatus" NOT NULL DEFAULT 'uploading',
+    "notebookId" TEXT NOT NULL,
+    "type" "FileType" NOT NULL DEFAULT 'pdf',
+
+    CONSTRAINT "Source_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "DocumentChunk" (
+    "id" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "page" INTEGER,
+    "chunkIndex" INTEGER NOT NULL,
+    "embedding" vector(1536) NOT NULL,
+    "sourceId" TEXT NOT NULL,
+    "parentIds" TEXT[],
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "DocumentChunk_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ParentChunk" (
+    "id" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "sourceId" TEXT NOT NULL,
+
+    CONSTRAINT "ParentChunk_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "Source_userId_idx" ON "Source"("userId");
+
+-- CreateIndex
+CREATE INDEX "Source_notebookId_idx" ON "Source"("notebookId");
+
+-- CreateIndex
+CREATE INDEX "DocumentChunk_sourceId_idx" ON "DocumentChunk"("sourceId");
+
+-- CreateIndex
+CREATE INDEX "ParentChunk_sourceId_idx" ON "ParentChunk"("sourceId");
+
+-- AddForeignKey
+ALTER TABLE "Source" ADD CONSTRAINT "Source_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Source" ADD CONSTRAINT "Source_notebookId_fkey" FOREIGN KEY ("notebookId") REFERENCES "notebook"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "DocumentChunk" ADD CONSTRAINT "DocumentChunk_sourceId_fkey" FOREIGN KEY ("sourceId") REFERENCES "Source"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ParentChunk" ADD CONSTRAINT "ParentChunk_sourceId_fkey" FOREIGN KEY ("sourceId") REFERENCES "Source"("id") ON DELETE CASCADE ON UPDATE CASCADE;
